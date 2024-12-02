044249f5f4a974395f06ddbb852b5df16b0fb794
hasanq1 <hasanq36@gmail.com>
2024-12-01 18:22:39 -0600

    Footer enhacements

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,19 +5,24 @@ import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
+
   return (
     <Fade bottom duration={1000} distance="5px">
+      <hr></hr>
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Rapid Development by the Nebulous Research team")}
+          {emoji("© 2024 Nebulous Research Inc. All rights reserved.")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {" "}
+          Designed and developed by the Nebulous Research team.
+        </p>
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           <a
-            href="https://github.com/saadpasta/developerFolio"
+            href="https://www.nebulousresearch.com"
             target="_blank"
             rel="noreferrer"
+            style={{ textDecoration: "none", color: isDark ? "#ffffff" : "#000000" }}
           >
             
           </a>
