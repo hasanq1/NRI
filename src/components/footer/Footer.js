a9387e767c83febd1fe516a1a54542f761952b2d
hasanq1 <hasanq36@gmail.com>
2024-12-01 18:19:43 -0600

    fix

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -13,13 +13,13 @@ export default function Footer() {
           {emoji("Rapid Development by the Nebulous Research team")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
+          {" "}
           <a
             href="https://github.com/saadpasta/developerFolio"
             target="_blank"
             rel="noreferrer"
           >
-            developerFolio
+            
           </a>
         </p>
       </div>
